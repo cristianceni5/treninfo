5890018e8414006255aac7781b14fc456b6f21c7
Cristian Ceni <cenicristian@yahoo.com>
2025-11-28 19:00:19 +0100

    altri fix delle api

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -2,4 +2,7 @@
 const serverless = require('serverless-http');
 const app = require('../../src/app');
 
-exports.handler = serverless(app);
+// Diciamo a serverless-http qual è il "prefisso" da togliere
+exports.handler = serverless(app, {
+  basePath: '/.netlify/functions/api',
+});
